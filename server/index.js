135a8d22a22aac2cc74bffaa91fea8f31a40bde2
Tyler <millert2420@gmail.com>
2018-12-15 16:19:33 -0700

    server running

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-//const axios = require('axios');
-import { json } from 'body-parser';
-import express from 'express';
-//const session = require('express-session');
+const express = require('express');
+const bodyParser = require('body-parser');
 //const massive = require('massive');
+//const session = require('express-session');
+//const axios = require('axios');
 
 require('dotenv').config();
 
 const app = express();
-app.use(json());
+app.use(bodyParser.json());
 
 const SERVER_PORT = process.env.SERVER_PORT || 4000;
 app.listen(SERVER_PORT, () => {
